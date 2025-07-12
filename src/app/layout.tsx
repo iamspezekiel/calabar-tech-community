00c40edd61f54b233e92bb89850896bce87f9bda
Sylvanus P. Ezekiel <sylvanuspezekiel@gmail.com>
2025-07-12 11:41:52 +0000

    remove isabichat.online livechat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,19 +33,6 @@ export default function RootLayout({
           <Footer />
         </div>
         <Toaster />
-        <Script id="isabichat-widget" strategy="lazyOnload">
-          {`
-            var Widget_API=Widget_API||{}, Widget_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src="https://isabichat.online/api/livechat/widget/89CK7S42S8KRU2E";
-            s1.charset="UTF-8";
-            s1.setAttribute("crossorigin","*");
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
       </body>
     </html>
   );
